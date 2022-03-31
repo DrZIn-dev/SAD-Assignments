e4ec557087896fc9cfe2ed5929c2473690d13fc0
DrZIn-dev <quick.car6020@fastmail.com>
2022-03-31 15:10:28 +0700

    feat(assignment_09): done

---
+++ assignment_09/src/structural/HealthcareWorkerDecorator.java
@@ -0,0 +1,20 @@
+package structural;
+
+public abstract class HealthcareWorkerDecorator extends HealthcareWorker{
+    private HealthcareWorker worker;
+
+    public HealthcareWorkerDecorator(HealthcareWorker worker) {
+        super(worker);
+        this.worker = worker;
+    }
+
+    @Override
+    public void service() {
+        this.worker.service();
+    }
+
+    @Override
+    public double getPrice() {
+        return this.worker.getPrice();
+    }
+}
---
+++ assignment_09/src/structural/HealthcareWorkerTeam.java
@@ -0,0 +1,37 @@
+package structural;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HealthcareWorkerTeam implements HealthcareServiceable{
+    private Set<HealthcareServiceable> members;
+
+    HealthcareWorkerTeam(){
+        members = new HashSet<HealthcareServiceable>();
+    }
+
+    public void addMember(HealthcareServiceable member){
+        members.add(member);
+    }
+
+    public void removeMember(HealthcareServiceable member){
+        members.remove(member);
+    }
+
+    public void service() {
+        for (HealthcareServiceable member : members
+        ) {
+            member.service();
+        }
+    }
+
+    public double getPrice() {
+        double sumPrice = 0.0;
+        for (HealthcareServiceable member : members
+        ) {
+            sumPrice += member.getPrice();
+        }
+        return sumPrice;
+    }
+
+}
