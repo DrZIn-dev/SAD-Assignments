56b21cc91e2ba2f0315217d59c447362f7a8ac7b
DrZIn-dev <quick.car6020@fastmail.com>
2022-04-07 20:02:08 +0700

    chore(assignment_10): copy original code

---
+++ assignment_10/src/behavioral/NewsPublisher.java
@@ -0,0 +1,8 @@
+package behavioral;
+
+import java.util.concurrent.Flow.Publisher;
+import java.util.concurrent.Flow.Subscriber;
+
+public class NewsPublisher implements Publisher<News> {
+	// You need to implement this class
+}
---
+++ assignment_10/src/behavioral/NewsSubscriber.java
@@ -0,0 +1,8 @@
+package behavioral;
+
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+
+public class NewsSubscriber implements Subscriber<News> {
+	// You need to implement this class
+}
