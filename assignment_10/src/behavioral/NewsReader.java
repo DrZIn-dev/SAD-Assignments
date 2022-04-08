08bcb49569550e66ae7f7fa38aff1088d9a66ec2
DrZIn-dev <quick.car6020@fastmail.com>
2022-04-08 14:24:33 +0700

    chore(assignment_10): remove class NewsSubscriber

--- assignment_10/src/behavioral/NewsReader.java
+++ assignment_10/src/behavioral/NewsReader.java
@@ -1,10 +1,38 @@
 package behavioral;
 
-import java.util.concurrent.Flow.Subscriber;
-import java.util.concurrent.Flow.Subscription;
+import java.util.concurrent.Flow;
 
-public class NewsReader extends NewsSubscriber{
-    NewsReader(String announcer){
-        super(announcer);
+public class NewsReader implements Flow.Subscriber<News> {
+    // You need to implement this class
+    private String announcer;
+
+    NewsReader(String announcer) {
+        this.announcer = announcer;
+    }
+
+    public void update(News news) {
+        System.out.println("announcer : " + this.announcer);
+        System.out.println("topic : " + news.getTopic());
+        System.out.println("content : " + news.getContent());
+        System.out.println("===============================");
+    }
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+
+    }
+
+    @Override
+    public void onNext(News item) {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+    }
+
+    @Override
+    public void onComplete() {
+
     }
 }
